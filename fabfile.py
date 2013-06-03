c41b6603400ffa8d12034b4bfa6b3d645d3a2927
Matt Griffiths <mrmatthewgriffiths@gmail.com>
2013-06-03 11:49:32 -0400

    ADHOC: UPdated submodules

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,7 @@
 from __future__ import with_statement
 import os
 from fabric.api import *
-from local_fabfile import LOCAL_CODE_ROOT
+from local_fabfile import *
 
 
 packages = {
@@ -11,7 +11,24 @@ packages = {
     ]
 }
 
+
 def git():
-    for directory in ['.']:
+    for directory in ['django_odc', '.']:
         cd_path = os.path.join(LOCAL_CODE_ROOT, directory)
         local('cd %s && git add --all && git commit && git push origin master' % cd_path)
+
+        
+def deploy(alert_branch='master', odc_branch='master'):
+    with cd('/usr/local/metaLayer-alert'):
+        run("git fetch")
+        run("git merge origin/%s" % alert_branch)
+        run("git status")
+    with cd('/usr/local/metaLayer-alert/django_odc'):
+        run("git fetch")
+        run("git merge origin/%s" % odc_branch)
+        run("git status")
+    with cd('/usr/local/metaLayer-alert'):
+        run("python manage.py collectstatic --noinput")
+        run("pip install -r requirements.txt")
+    with settings(warn_only=True):
+        run("service apache2 restart")
