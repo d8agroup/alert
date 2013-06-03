8fbdd327aa7074a7b33646bf658fff49e7740c70
Matt Griffiths <mrmatthewgriffiths@gmail.com>
2013-06-03 09:41:11 -0400

    ADHOC: Added django_odc submodule

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,7 @@
 from __future__ import with_statement
 import os
 from fabric.api import *
+from local_fabfile import LOCAL_CODE_ROOT
 
 
 packages = {
@@ -13,7 +14,7 @@ packages = {
     ]
 }
 
-
 def git():
-    for dir in ['/.']:
-        local('cd ~/code/metaLayer/alert%s && git add --all && git commit && git push origin master' % dir)
+    for directory in ['.']:
+        cd_path = os.path.join(LOCAL_CODE_ROOT, directory)
+        local('cd %s && git add --all && git commit && git push origin master' % cd_path)
